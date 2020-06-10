bba5dd8a832f44424349f7cfa5cdd871b86f0578
hrushikesh09 <hrushikeshshinde11@yahoo.com>
2020-06-11 00:43:43 +0530

    test changes

--- config.mk
+++ config.mk
@@ -1,14 +1,8 @@
 ###############################################################################
-### Reaction Platform Configuration
+### Reaction OSS v3.0.0
 ###
-### This file defines configuration used in the Makefile.
-### You may add and/or override these values with your own custom configuration
-### in `config.local.mk`.
-###
-### Please see GNU Makes multi-line variable documentation for more info.
-### https://www.gnu.org/software/make/manual/html_node/Multi_002dLine.html
+### See: `/config.mk` for documentation.
 ###############################################################################
-
 
 # List of tools that must be installed.
 # A simple check to determine the tool is available. No version check, etc.
@@ -28,54 +22,10 @@ endef
 # Projects will be started in this order
 define SUBPROJECT_REPOS
 https://github.com/reactioncommerce/reaction-hydra.git,reaction-hydra,v3.0.0 \
-https://github.com/reactioncommerce/reaction.git,reaction,v3.8.0 \
-https://github.com/reactioncommerce/reaction-identity.git,reaction-identity,v3.1.0 \
-https://github.com/reactioncommerce/reaction-admin.git,reaction-admin,v3.0.0-beta.7 \
-https://github.com/reactioncommerce/example-storefront.git,example-storefront,v3.1.0
-endef
-
-# These are all the plugins that `make clone-api-plugins` will clone.
-# `api-core` is not technically a plugin but is useful to have in the
-# same place.
-define API_PLUGIN_REPOS
-https://github.com/reactioncommerce/api-core.git \
-https://github.com/reactioncommerce/api-plugin-accounts.git \
-https://github.com/reactioncommerce/api-plugin-address-validation-test.git \
-https://github.com/reactioncommerce/api-plugin-address-validation.git \
-https://github.com/reactioncommerce/api-plugin-authentication.git \
-https://github.com/reactioncommerce/api-plugin-authorization-simple.git \
-https://github.com/reactioncommerce/api-plugin-carts.git \
-https://github.com/reactioncommerce/api-plugin-catalogs.git \
-https://github.com/reactioncommerce/api-plugin-discounts-codes.git \
-https://github.com/reactioncommerce/api-plugin-discounts.git \
-https://github.com/reactioncommerce/api-plugin-email-smtp.git \
-https://github.com/reactioncommerce/api-plugin-email-templates.git \
-https://github.com/reactioncommerce/api-plugin-email.git \
-https://github.com/reactioncommerce/api-plugin-files.git \
-https://github.com/reactioncommerce/api-plugin-i18n.git \
-https://github.com/reactioncommerce/api-plugin-inventory-simple.git \
-https://github.com/reactioncommerce/api-plugin-inventory.git \
-https://github.com/reactioncommerce/api-plugin-job-queue.git \
-https://github.com/reactioncommerce/api-plugin-navigation.git \
-https://github.com/reactioncommerce/api-plugin-notifications.git \
-https://github.com/reactioncommerce/api-plugin-orders.git \
-https://github.com/reactioncommerce/api-plugin-payments-example.git \
-https://github.com/reactioncommerce/api-plugin-payments-stripe.git \
-https://github.com/reactioncommerce/api-plugin-payments.git \
-https://github.com/reactioncommerce/api-plugin-pricing-simple.git \
-https://github.com/reactioncommerce/api-plugin-products.git \
-https://github.com/reactioncommerce/api-plugin-settings.git \
-https://github.com/reactioncommerce/api-plugin-shipments-flat-rate.git \
-https://github.com/reactioncommerce/api-plugin-shipments.git \
-https://github.com/reactioncommerce/api-plugin-shops.git \
-https://github.com/reactioncommerce/api-plugin-simple-schema.git \
-https://github.com/reactioncommerce/api-plugin-sitemap-generator.git \
-https://github.com/reactioncommerce/api-plugin-surcharges.git \
-https://github.com/reactioncommerce/api-plugin-system-information.git \
-https://github.com/reactioncommerce/api-plugin-tags.git \
-https://github.com/reactioncommerce/api-plugin-taxes-flat-rate.git \
-https://github.com/reactioncommerce/api-plugin-taxes.git \
-https://github.com/reactioncommerce/api-plugin-translations.git
+https://github.com/reactioncommerce/reaction.git,reaction,v3.0.0 \
+https://github.com/reactioncommerce/reaction-identity.git,reaction-identity,v3.0.0 \
+https://github.com/reactioncommerce/reaction-admin.git,reaction-admin,v3.0.0-beta.4 \
+https://github.com/sevdashubham/medical-commerce.git,example-storefront,trunk
 endef
 
 # List of user defined networks that should be created.
